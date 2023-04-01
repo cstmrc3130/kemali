05d16adbed0e60ee830a117194bc60d80cd93bf3
CAESTO MARCO TAMPUBOLON <marcocaesto@gmail.com>
2023-04-01 13:54:27 +0700

    Optimizing Not Found Message

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ $(function ()
     const liElements = $("#search-lists").children('li');
     const defaultState = $("#search-lists").html();
 
-    $("#search-box").on('keyup', function (e)
+    $("#search-box").on('input', function (e)
     {
         const keyword = e.target.value.toLowerCase();
-        const elements = new Array();
+        const matchingElements = [];
+        const nonMatchingElements = [];
         const MINIMAL_DISTANCE = 3;
 
         const Distance = (e) =>
@@ -18,56 +19,68 @@ $(function ()
 
         if (keyword !== "")
         {
-            $.each(liElements, (key, element) => 
+            $.each(liElements, (key, li) => 
             {
-                const elementText = element.innerText.toLowerCase().replace(/[0-9]/g, '').trim();
+                const elementText = li.innerText.toLowerCase().replace(/[0-9]/g, '').trim();
 
                 // FIRST CHECK EACH ITEM OVERALL SIMILARITY USING contains()
                 if (elementText.includes(keyword))
                 {
-                    $("#not-found").hasClass("is-hidden") ? "" : $("#not-found").addClass("is-hidden")
-
                     // CHANGE TOTAL CHARACTER DIFFERENCES (DISTANCE)
-                    $(element.children).each(function ()
+                    $(li.children).each(function ()
                     {
                         $(this).children('span').last().html(levenhstein(keyword, elementText));
                     });
 
-                    // element.style.display = "block"
-                    elements.push(element);
+                    matchingElements.push(li);
                 }
                 // IF CHARACTERS ARE OUT OF TRACK, THEN CHECK USING levenhstein() TO COUNT CHARACTER DIFFERENCES
                 else
                 {
                     if (levenhstein(keyword, elementText) <= MINIMAL_DISTANCE)
                     {
-                        $("#not-found").hasClass("is-hidden") ? $("#not-found").removeClass("is-hidden") : ""
-
                         // CHANGE TOTAL CHARACTER DIFFERENCES
-                        $(element.children).each(function ()
+                        $(li.children).each(function ()
                         {
                             $(this).children('span').last().html(levenhstein(keyword, elementText));
                         });
 
-                        // element.style.display = "block"
-                        elements.push(element);
+                        nonMatchingElements.push(li);
                     }
                 }
             })
 
-            // SORT THE ELEMENTS BY COMPARING THE DISTANCE
-            elements.sort((a, b) => Distance(a) - Distance(b));
-
             $("#search-lists").html("");
 
-            $.each(elements, (key, value) =>
+            if (matchingElements.length > 0 || matchingElements.length > 0 && nonMatchingElements.length > 0)
             {
-                $("#search-lists").append(value);
-            })
+                $("#not-found").hasClass("is-hidden") ? "" : $("#not-found").addClass("is-hidden")
+
+                // SORT THE ELEMENTS BY COMPARING THE DISTANCE
+                matchingElements.sort((a, b) => Distance(a) - Distance(b));
+
+                $.each(matchingElements, (key, value) =>
+                {
+                    $("#search-lists").append(value);
+                })
+            }
+            else
+            {
+                $("#not-found").hasClass("is-hidden") ? $("#not-found").removeClass("is-hidden") : ""
+
+                // SORT THE ELEMENTS BY COMPARING THE DISTANCE
+                nonMatchingElements.sort((a, b) => Distance(a) - Distance(b));
+
+                $.each(nonMatchingElements, (key, value) =>
+                {
+                    $("#search-lists").append(value);
+                })
+            }
         }
         else
         {
             $("#search-lists").html(defaultState)
+            $("#not-found").addClass("is-hidden")
         }
     })
 })
